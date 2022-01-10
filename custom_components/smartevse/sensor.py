06204ff0c8af49b49d4c177729ee4f69dce6b988
Kenny Schuoler <kenny@Kennys-MacBook-Air.local>
2022-01-10 16:18:10 +0100

    Save values

--- custom_components/smartevse/sensor.py
+++ custom_components/smartevse/sensor.py
@@ -1,6 +1,6 @@
 import logging
 
-from typing import Callable
+from typing import Callable, Optional
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.components.sensor import DEVICE_CLASSES, SensorEntity
@@ -22,28 +22,49 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 
     sensor_list = []
 
-    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "State", "_evse_status", 0, None, None))
-    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Error", "_evse_status", 1, None, None))
-    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Charging current", "_evse_status", 2, None, None))
-    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "EVSE mode (without saving)", "_evse_status", 3, None, None))
-    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Solar Timer", "_evse_status", 4, None, None))
-    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Access bit", "_evse_status", 5, None, None))
-#     sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Configuration changed (Not implemented)", "_evse_status", 6, None, None))
-    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Maximum charging current", "_evse_status", 7, None, None))
-#     sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Number of used phases (Not implemented)", "_evse_status", 8, None, None))
-#     sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Real charging current (Not implemented)", "_evse_status", 9, None, None))
-    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Temperature", "_evse_status", 10, None, None))
-    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Serial number", "_evse_status", 11, None, None))
+    # Device classes : https://www.home-assistant.io/integrations/sensor/
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "State", "_evse_status", 0, None, None, {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Error", "_evse_status", 1, None, None, {}, { "0": "", "1": "LESS_6A", "2": "NO_COMM", "4": "TEMP_HIGH", "8": "Unused", "16": "RCD", "32": "NO_SUN" }))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Charging current", "_evse_status", 2, "A", "current", { "precision": 1 }, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "EVSE mode (without saving)", "_evse_status", 3, None, None, {}, { "0": "Normal", "1": "Smart", "2": "Solar" }))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Solar Timer", "_evse_status", 4, "s", None, {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Access bit", "_evse_status", 5, None, None, {}, { "0": "No Access", "1": "Access" }))
+#     sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Configuration changed (Not implemented)", "_evse_status", 6, None, None, {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Maximum charging current", "_evse_status", 7, "A", "current", {}, {}))
+#     sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Number of used phases (Not implemented)", "_evse_status", 8, None, None, {}, {}))
+#     sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Real charging current (Not implemented)", "_evse_status", 9, "A", "current", {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Temperature", "_evse_status", 10, "°C", "temperature",  { "offset": "-273.16" }, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Serial number", "_evse_status", 11, None, None, {}, {}))
 
-#     node_specific_config = getattr(coordinator.data, "node_specific_config")
-#     system_config = getattr(coordinator.data, "system_config")
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Configuration", "_node_specific_config", 0, None, None, {}, { "0": "Socket", "1": "Fixed Cable" }))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Cable lock", "_node_specific_config", 1, None, None, {}, { "0": "Disable", "1": "Solenoid", "2": "Motor" }))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "MIN Charge Current the EV will accept", "_node_specific_config", 2, "A", "current", {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "MAX Charge Current for this EVSE", "_node_specific_config", 3, "A", "current", {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Load Balance", "_node_specific_config", 4, None, None, {}, { "0": "Disabled", "1": "Master", "2": "Node 2", "3": "Node 3", "4": "Node 4", "5": "Node 5", "6": "Node 6", "7": "Node 7", "8": "Node 8" }))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "External Switch on pin SW", "_node_specific_config", 5, None, None, {}, { "0": "Disabled", "1": "Access Push-Button", "2": "Access Switch", "3": "Smart-Solar Push-Button", "4": "Smart-Solar Switch" }))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Residual Current Monitor on pin RCM", "_node_specific_config", 6, None, None, {}, { "0": "Disabled", "1": "Enabled" }))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Use RFID reader", "_node_specific_config", 7, None, None, {}, { "0": "Disabled", "1": "Enabled" }))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Type of EV electric meter", "_node_specific_config", 8, None, None, {}, {}))
+#     sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Address of EV electric meter", "_node_specific_config", 9, None, None, {}, {}))
+
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "EVSE mode", "_system_config", 0, None, None, {}, { "0": "Normal", "1": "Smart", "2": "Solar" }))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "EVSE Circuit max Current", "_system_config", 1, "A", "current", {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Grid type to which the Sensorbox is connected", "_system_config", 2, None, None, {}, { "0": "4-Wire", "1": "3-Wire" }))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "CT calibration value", "_system_config", 3, None, None, { "precision": 2 }, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Max Mains Current", "_system_config", 4, "A", "current", {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Surplus energy start Current", "_system_config", 5, "A", "current", {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Stop solar charging at 6A after this time	", "_system_config", 6, "min", None, {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Allow grid power when solar charging", "_system_config", 7, "A", "current", {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Type of Mains electric meter", "_system_config", 8, None, None, {}, {}))
+#     sensor_list.append(SmartEvseSensor(coordinator, entry.data, "Address of Mains electric meter", "_system_config", 9, None, None, {}, {}))
+    sensor_list.append(SmartEvseSensor(coordinator, entry.data, "What does Mains electric meter measure", "_system_config", 10, None, None, {}, { "0": "Mains (Home + EVSE + PV)", "1": "Home + EVSE" }))
 
     async_add_entities(sensor_list)
 
 class SmartEvseSensor(CoordinatorEntity, SensorEntity):
     """Representation of a Smart EVSE Sensor."""
 
-    def __init__(self, coordinator, charging_station, sensor_name, sensor_value_category_key, sensor_value_item_key, device_class, unit_of_measurement):
+    def __init__(self, coordinator, charging_station, sensor_name, sensor_value_category_key, sensor_value_item_key, unit_of_measurement, device_class, properties={}, friendly_sensor_values={}):
         """Pass coordinator to CoordinatorEntity."""
         super().__init__(coordinator)
         self._charging_station = charging_station
@@ -53,6 +74,8 @@ class SmartEvseSensor(CoordinatorEntity, SensorEntity):
         self._sensor_value = None
         self._device_class = device_class
         self._unit_of_measurement = unit_of_measurement
+        self._properties = properties
+        self._friendly_sensor_values = friendly_sensor_values
 
     @property
     def unique_id(self) -> str:
@@ -76,14 +99,15 @@ class SmartEvseSensor(CoordinatorEntity, SensorEntity):
         if self._sensor_value_category_key in self.coordinator.data:
             category = self.coordinator.data[self._sensor_value_category_key]
             if 0 <= self._sensor_value_item_key < len(category):
-                new_sensor_value = category[self._sensor_value_item_key]
+                new_sensor_value = round(float(category[self._sensor_value_item_key] + self.offset()), self.precision())
+                if str(new_sensor_value) in self._friendly_sensor_values:
+                    new_sensor_value = self._friendly_sensor_values[str(new_sensor_value)]
                 _LOGGER.debug(f"{DOMAIN_FRIENDLY} : Updating sensor value from {self._sensor_value} to {new_sensor_value}")
                 self._sensor_value = new_sensor_value
             else:
                 _LOGGER.warn(f"{DOMAIN_FRIENDLY} : Index '{self._sensor_value_item_key}' in category '{self._sensor_value_category_key}' is not defined")
         else:
             _LOGGER.warn(f"{DOMAIN_FRIENDLY} : Category key '{self._sensor_value_category_key}' not defined")
-
 
     @property
     def unit_of_measurement(self) -> str:
@@ -96,9 +120,21 @@ class SmartEvseSensor(CoordinatorEntity, SensorEntity):
         return self._device_class
 
     @property
-    def state_class(self) -> str:
+    def state_class(self) -> Optional[str]:
         """Return the state_class for the sensor, to enable statistics"""
-        return None
+        if not self._device_class:
+            return None
+        return "measurement"
+
+    def offset(self) -> float:
+        if "offset" in self._properties:
+            return float(self._properties["offset"])
+        return 0
+
+    def precision(self) -> int:
+        if "precision" in self._properties:
+            return int(self._properties["precision"])
+        return 0
 
     @property
     def device_info(self) -> dict:
